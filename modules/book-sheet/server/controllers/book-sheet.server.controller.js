4fd2ebc5d3025abc87897ea6d76e1ebfd12e0d2c
Saber0517 <zzbond0517@gmail.com>
2016-04-30 21:31:13 +0800

    add server side crud function & test

--- modules/book-sheet/server/controllers/book-sheet.server.controller.js
+++ modules/book-sheet/server/controllers/book-sheet.server.controller.js
@@ -5,6 +5,7 @@
  */
 var path = require('path'),
   mongoose = require('mongoose'),
+  Post = mongoose.model('Post'),
   errorHandler = require(path.resolve('./modules/core/server/controllers/errors.server.controller')),
   _ = require('lodash');
 
@@ -12,7 +13,19 @@ var path = require('path'),
  * Create a Book sheet
  */
 exports.create = function (req, res) {
+  var post = new Post(req.body);
+  post.author = req.user;
 
+  post.save(function (err) {
+    if (err) {
+      console.log(err);
+      return res.status(400).send({
+        message: errorHandler.getErrorMessage(err)
+      });
+    } else {
+      res.json(post);
+    }
+  });
 };
 
 /**
@@ -33,12 +46,54 @@ exports.update = function (req, res) {
  * Delete an Book sheet
  */
 exports.delete = function (req, res) {
-
+  var post = req.post;
+  post.remove(function (err) {
+    if (err) {
+      return res.status(400).send({
+        message: errorHandler.getErrorMessage(err)
+      });
+    } else {
+      res.json(post);
+    }
+  });
 };
 
 /**
  * List of Book sheets
  */
 exports.list = function (req, res) {
+  Post.find().populate('author', 'displayName').exec(function (err, posts) {
+    if (err) {
+      console.log(err);
+      return res.status(400).send({
+        message: errorHandler.getErrorMessage(err)
+      });
+    } else {
+      res.json(posts);
+    }
+  });
+};
 
+/*
+* Post Middleware
+*/
+exports.postByID = function (req, res, next, id) {
+  if(!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).send({
+      message: 'Post ID is invalid'
+    });
+  }
+
+  Post.findById(id).populate('author', 'displayName').exec(function (err, posts) {
+    if (err) {
+      return next(err);
+    }else if(!posts){
+      return res.status(400).send({
+        message: 'No post with that identifier has been found'
+      });
+    }
+    req.post = posts;
+    next();
+
+  });
 };
--- modules/book-sheet/tests/server/post.server.routes.tests.js
+++ modules/book-sheet/tests/server/post.server.routes.tests.js
@@ -1,56 +1,159 @@
 'use strict';
 
 var should = require('should'),
-    request = require('supertest'),
-    path = require('path'),
-    mongoose = require('mongoose'),
-    User = mongoose.model('User'),
-    Post = mongoose.model('Post'),
-    express = require(path.resolve('./config/lib/epxress'));
+  request = require('supertest'),
+  path = require('path'),
+  mongoose = require('mongoose'),
+  User = mongoose.model('User'),
+  Post = mongoose.model('Post'),
+  express = require(path.resolve('./config/lib/express'));
 
 //global variable
 var app, agent, credentials, user, post;
 
 //posts routes tests
 describe('Post CRUD test', function () {
-    before(function (done) {
-        app = express.init(mongoose);
-        agent = request.agent(app);
-        done();
+  before(function (done) {
+    app = express.init(mongoose);
+    agent = request.agent(app);
+    done();
+  });
+
+  beforeEach(function (done) {
+    credentials = {
+      username: 'james zhang',
+      password: 'ZXcv_123456'
+    };
+
+    user = new User({
+      firstName: 'James',
+      lastName: 'Zhang',
+      displayName: 'James Zhang',
+      email: 'test@test.com',
+      username: credentials.username,
+      password: credentials.password,
+      provider: 'local'
     });
 
-    beforeEach(function(done){
-        credentials = {
-            username :'MEANJSUser',
-            password : 's@mplePassword'
-        };
+    user.save(function () {
+      post = {
+        title: 'Sample Post',
+        text: 'Hello World'
+      };
 
-        user = new User({
-            firstName :'MEAN',
-            lastName : 'JS',
-            displayName : 'MEANJS',
-            email:'meanjs@test.com',
-            username : credentials.username,
-            password : credentials.password,
-            provider : 'local'
+      done();
+
+    });
+  });
+
+  //Http POST  to the /api/posts
+  it('should be able to save an post if logged in', function (done) {
+    agent.post('/api/auth/signin')
+      .send(credentials)
+      .expect(200)
+      .end(function (signInErr, signInRes) {
+        if (signInErr) {
+          return done(signInErr);
+        }
+
+        var userId = user.id;
+
+        agent.post('/api/posts')
+          .send(post)
+          .expect(200)
+          .end(function (postSaveErr, postSaveRes) {
+            if (postSaveErr) {
+              return done(postSaveErr);
+            }
+            Post.find().exec(function (err, posts) {
+              if (err) {
+                return done(err);
+              }
+              (posts[0].author.toString()).should.equal(userId);
+              (posts[0].title).should.match('Sample Post');
+              done();
+            });
+
+            /*agent.get('/api/posts')
+             .end(function (postsGetErr, postsGetRes) {
+             if (postsGetErr) {
+             return done(postsGetErr);
+             }
+
+             var posts = postsGetRes.body;
+
+             (posts[0].user._id).should.equal(userId);
+             (posts[0].title).should.match('Sample Post');
+
+             done();
+             });
+             });*/
+          });
+      });
+  });
+
+  //Http GET /api/posts
+  it('should be able to get a list of posts if logged in', function (done) {
+    var postObj = new Post(post);
+    postObj.save(function () {
+      agent.post('/api/auth/signin')
+        .send(credentials)
+        .expect(200)
+        .end(function (signInErr, signInRes) {
+          if (signInErr) {
+            return done(signInErr);
+          }
+
+          agent.get('/api/posts')
+            .expect(200)
+            .end(function (postGetErr, postGetRes) {
+              if (postGetErr) {
+                return done(postGetErr);
+              }
+              postGetRes.body.should.be.instanceof(Array).and.have.lengthOf(1);
+
+              done();
+            });
         });
 
-        user.save(function () {
-            post = {
-                title:'Sample Post',
-                text:'Hello World'
-            };
+    });
+  });
+  //Http GET /api/posts/:postID
 
-            done();
+  //Http DELETE /api/posts/:postID
+  it('should be able to delete a post if logged in', function (done) {
+    var postObj = new Post(post);
+    postObj.save(function () {
+      agent.post('/api/auth/signin')
+        .send(credentials)
+        .expect(200)
+        .end(function (signInErr, signInRes) {
+          if(signInErr){
+            return done(signInErr);
+          }
 
+          agent.delete('/api/posts/' + postObj._id)
+            .expect(200)
+            .end(function (postDeleteErr, postDeleteRes) {
+              if(postDeleteErr){
+                return done(postDeleteErr);
+              }
+
+              (postDeleteRes.body._id).should.equal(postObj._id.toString());
+
+              done();
+            });
         });
     });
+  });
+  //Http PUT /api/posts/:postID
 
-    afterEach(function (done) {
-        User.remove().exec(function () {
-            Post.remove().exec(done);
-        })
+
+  afterEach(function (done) {
+    User.remove().exec(function () {
+      Post.remove().exec(done);
     });
+  });
 
 });
 
